6988cff0338108111cbbec64e0977208200ec78b
nalavala <poojinvr@gmail.com>
2020-03-18 08:15:31 +0530

    Updates

---
+++ precache-manifest.653179ef1ce1912e18ff0d1634444cbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b9bda8048eb0ace36df9c1178fe748",
+    "url": "/quickapp/index.html"
+  },
+  {
+    "revision": "a0241b9cf44535d05697",
+    "url": "/quickapp/static/css/main.5e36f410.chunk.css"
+  },
+  {
+    "revision": "c5fcf681b80182b932c2",
+    "url": "/quickapp/static/js/2.87d22aa9.chunk.js"
+  },
+  {
+    "revision": "84c7b19269f6e118b33bddb7c7a89789",
+    "url": "/quickapp/static/js/2.87d22aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0241b9cf44535d05697",
+    "url": "/quickapp/static/js/main.f648a459.chunk.js"
+  },
+  {
+    "revision": "a04a7e5521f99be1dbc4",
+    "url": "/quickapp/static/js/runtime-main.fa25083a.js"
+  },
+  {
+    "revision": "1aeddf3704234af9b0d75ee2df67ecfc",
+    "url": "/quickapp/static/media/awesome hands.1aeddf37.JPG"
+  }
+]);
